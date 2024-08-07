1cde09489eb172244d44156ae1e1fda7e455c5a8
bcamastro <79494854+bcamastro@users.noreply.github.com>
2024-08-07 18:16:04 -0400

    added a 4th OOP practice
    I only did one today, as today was mostly a study day after work.

--- oopPractice.py
+++ oopPractice.py
@@ -30,12 +30,51 @@
 
 #expected output: Vehicle Name: School Volvo Speed: 180 Mileage: 12
 
-class vehicle:
+# class vehicle:
+#     def __init__(self, name, max_speed, mileage):
+#         self.name = name
+#         self.max_speed = max_speed
+#         self.mileage = mileage
+
+# class bus(vehicle):
+#     pass
+# School_bus = Bus("School Volvo", 180, 12)
+
+
+
+
+
+
+
+# OOP Exercise 4: Class Inheritance
+# Create a Bus class that inherits from the Vehicle class. Give the capacity argument of Bus.seating_capacity() a default value of 50.
+# Use the following code for your parent Vehicle class.
+
+# class Vehicle:
+#     def __init__(self, name, max_speed, mileage):
+#         self.name = name
+#         self.max_speed = max_speed
+#         self.mileage = mileage
+
+#     def seating_capacity(self, capacity):
+#         return f"The seating capacity of a {self.name} is {capacity} passengers"
+
+class Vehicle:
     def __init__(self, name, max_speed, mileage):
         self.name = name
         self.max_speed = max_speed
         self.mileage = mileage
 
-class bus(vehicle):
-    pass
-School_bus = Bus("School Volvo", 180, 12)
+    def seating_capacity(self, capacity):
+        return f"The seating capacity of a {self.name} is {capacity}"
+    
+class Bus(Vehicle):
+    def seating_capacity(self, capacity=50):
+        return super().seating_capacity(capacity=50)
+    
+bus1 = Bus("bus1", 85, 32000)
+print(bus1.seating_capacity())
+
+#for this one I changed based off the answer board. I coded it a different way, not using the super() function, however after
+#reviewing the answer sheet, I realized using super would make sense here since this code is implying more sibling classes may
+# be added and need to use inheritance, so to avoid future errors, super() is the proper way.
