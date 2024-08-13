1e6743f7e760ab396fff3a66f7e30080d5086fa0
bcamastro <79494854+bcamastro@users.noreply.github.com>
2024-08-13 18:47:25 -0400

    Added a 5th practice.

--- oopPractice.py
+++ oopPractice.py
@@ -59,22 +59,64 @@
 #     def seating_capacity(self, capacity):
 #         return f"The seating capacity of a {self.name} is {capacity} passengers"
 
+# class Vehicle:
+#     def __init__(self, name, max_speed, mileage):
+#         self.name = name
+#         self.max_speed = max_speed
+#         self.mileage = mileage
+
+#     def seating_capacity(self, capacity):
+#         return f"The seating capacity of a {self.name} is {capacity}"
+    
+# class Bus(Vehicle):
+#     def seating_capacity(self, capacity=50):
+#         return super().seating_capacity(capacity=50)
+    
+# bus1 = Bus("bus1", 85, 32000)
+# print(bus1.seating_capacity())
+
+#for this one I changed based off the answer board. I coded it a different way, not using the super() function, however after
+#reviewing the answer sheet, I realized using super would make sense here since this code is implying more sibling classes may
+# be added and need to use inheritance, so to avoid future errors, super() is the proper way.
+
+
+
+
+
+# OOP Exercise 5: Define a property that must have the same value for every class instance (object)
+# Define a class attribute”color” with a default value white. I.e., Every Vehicle should be white.
+
+# Use the following code for this exercise.
+
+# class Vehicle:
+
+#     def __init__(self, name, max_speed, mileage):
+#         self.name = name
+#         self.max_speed = max_speed
+#         self.mileage = mileage
+
+# class Bus(Vehicle):
+#     pass
+
+# class Car(Vehicle):
+#     pass
+
+#expected output: 
+# Color: White, Vehicle name: School Volvo, Speed: 180, Mileage: 12
+# Color: White, Vehicle name: Audi Q5, Speed: 240, Mileage: 18
+
 class Vehicle:
+
+    color = "white"
+
     def __init__(self, name, max_speed, mileage):
         self.name = name
         self.max_speed = max_speed
         self.mileage = mileage
 
-    def seating_capacity(self, capacity):
-        return f"The seating capacity of a {self.name} is {capacity}"
-    
 class Bus(Vehicle):
-    def seating_capacity(self, capacity=50):
-        return super().seating_capacity(capacity=50)
-    
-bus1 = Bus("bus1", 85, 32000)
-print(bus1.seating_capacity())
+    pass
 
-#for this one I changed based off the answer board. I coded it a different way, not using the super() function, however after
-#reviewing the answer sheet, I realized using super would make sense here since this code is implying more sibling classes may
-# be added and need to use inheritance, so to avoid future errors, super() is the proper way.
+class Car(Vehicle):
+    pass
+
